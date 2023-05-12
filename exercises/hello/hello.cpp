b8cc9843b56e6d7c5f2f44e98f39c974297ab244
Anushka  Kulkarni <anushkakulk@login-students.khoury.northeastern.edu>
2023-05-12 00:26:21 -0400

    edited hello to test out committing

--- exercises/hello/hello.cpp
+++ exercises/hello/hello.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main(void)
 {
  //cout<<"Hello world! "<<endl;
- cout<<"Hello world! "+to_string(123)<<endl;
+ cout<<"Hello world! "+to_string(123) <<endl;
+
+ cout<<"I am editing this hello file to see if I can commit!"<<endl;
 }
