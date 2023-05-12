010edc8bde44e646149ca7197731172ed89b6e21
Anushka  Kulkarni <anushkakulk@login-students.khoury.northeastern.edu>
2023-05-12 01:02:02 -0400

    testing commit from vscode

--- exercises/hello/hello.cpp
+++ exercises/hello/hello.cpp
@@ -5,6 +5,4 @@ int main(void)
 {
  //cout<<"Hello world! "<<endl;
  cout<<"Hello world! "+to_string(123) <<endl;
-
- cout<<"I am editing this hello file to see if I can commit!"<<endl;
 }
---
+++ test/testing.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+ for (int i = 1; i <= 3; i+=1) {
+   cout<<"Testing "+to_string(i) <<endl;
+ }
+}
