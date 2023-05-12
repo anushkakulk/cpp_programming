30572e32a34e7e2fa957121d8b56e05406a593fa
Anushka Kulkarni <kulkarni.anushka@northeastern.edu>
2023-05-12 14:24:36 -0400

    included practice activities

---
+++ practice_activities/pa2/pa2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // prints out "anushka" using characters
+    cout << "                          _     _           " << endl;
+    cout << "   __ _ _ __  _   _  ___ | |__ | | __ __ _  " << endl;
+    cout << "  / _` | `_ \\| | | |/ __|| '_ \\| |/ // _` | " << endl;
+    cout << " | (_| | | | | |_| |\\__ \\| | | |   <| (_| | " << endl;
+    cout << "  \\__,_|_| |_|\\__,_||___/|_| |_|_|\\ ___,_| " << endl;
+}
