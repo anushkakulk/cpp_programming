fc860504076c113d474b07f87f84a7615cb2ad29
Anushka Kulkarni <kulkarni.anushka@northeastern.edu>
2023-05-12 14:57:27 -0400

    fully completed pa2

---
+++ pract_acts/pa2/pa2.cpp
@@ -0,0 +1,15 @@
+// Anushka Kulkarni
+// 5/12/2024
+// C++ program to print out "anushka" using ascii characters
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    
+    cout << "                          _     _           " << endl;
+    cout << "   __ _ _ __  _   _  ___ | |__ | | __ __ _  " << endl;
+    cout << "  / _` | `_ \\| | | |/ __|| '_ \\| |/ // _` | " << endl;
+    cout << " | (_| | | | | |_| |\\__ \\| | | |   <| (_| | " << endl;
+    cout << "  \\__,_|_| |_|\\__,_||___/|_| |_|_|\\ ___,_| " << endl;
+}
